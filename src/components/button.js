d459ac21f51634a2aa19675f6aa0210312da215c
GHOST-Aram <owinoojala@gmail.com>
2023-04-18 12:40:41 +0300

    feat: Create and Render Edit Button

--- src/components/button.js
+++ src/components/button.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const Button = ({Icon, handleClick, textClass}) => {
   return (
-    <button className = {`bg-gray ${textClass}  text-lg py-4 px-8 border-none rounded-md`}
+    <button className = {`bg-gray ${textClass}  text-lg py-4 px-8 border-none rounded-md mx-8`}
     onClick={handleClick}>
         <Icon/>    
       </button>
--- src/components/task.js
+++ src/components/task.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import Button from './button'
 import {FaTimes} from 'react-icons/fa'
+import {FaPen} from 'react-icons/fa'
 
-const Task = ({index, task, deleteTask}) => {
+const Task = ({index, task, deleteTask, editTask}) => {
   return (<li key={task.id} className='flex flex-row justify-between items-center text-green-500 py-4 px-2 border-b-1 border-light-gray'>
     <h3 className='font-light'>{index + 1}.  {task.text}</h3>
-    <Button Icon={FaTimes} handleClick={() => deleteTask(task.id)} textClass={'text-red-500'}/>
+    <div>
+      <Button Icon={FaPen} handleClick={() => editTask(task.id)} textClass={'text-yellow-500'}/>
+      <Button Icon={FaTimes} handleClick={() => deleteTask(task.id)} textClass={'text-red-500'}/>
+    </div>
     </li>)
 }
 
